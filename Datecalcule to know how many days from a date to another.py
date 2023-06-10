eb13916e50ae30a3bf863c960ddebdcc87aebf45
LucyToastinne <130234165+lucytoastinne484@users.noreply.github.com>
2023-06-10 14:49:43 -0300

    Add files via upload

---
+++ Datecalcule to know how many days from a date to another.py
@@ -0,0 +1,21 @@
+from datetime import date
+from datetime import datetime
+
+
+d1_alt = int(input("Digit a date you want to see: "))
+d2_alt = int(input("Digit another date too:"))
+print(d1_alt - d2_alt)
+
+dt1_alt = int(input("Digit a datetime you want to see:"))
+dt2_alt = int(input("Digit another datetime too:"))
+print(dt1_alt - dt2_alt)
+
+#d1 = date(2020, 11, 4)
+#d2 = date(2019, 11, 4)
+
+#print(d1 - d2)
+
+#dt1 = datetime(2020, 11, 4, 0, 0, 0)
+#dt2 = datetime(2019, 11, 4, 14, 53, 0)
+
+#print(dt1-dt2)
